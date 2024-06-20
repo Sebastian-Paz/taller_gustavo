902af50560381850cde36cdec14746bf384d9876
sebastian delgado <esdp5432@gmail.com>
2024-06-20 00:17:12 -0500

    agregando archivos

---
+++ src/Main.kt
@@ -0,0 +1,96 @@
+package org.example
+
+//********** primer paso *******
+/*fun main() {
+    println(1 == 1)
+}*/
+
+//Segundo paso
+/*fun main() {
+    println(1 < 1)
+}*/
+
+// ********* tercer paso ********
+/*fun main() {
+    val trafficLightColor = "red"
+}*/
+
+//*******CUARTO PASO**********
+
+/*fun main() {
+    val trafficLightColor = "red"
+
+    if (trafficLightColor == "red") {
+
+    }
+}*/
+
+//******* Quinto paso *********
+
+/*fun main() {
+    val trafficLightColor = "red"
+
+    if (trafficLightColor == "red") {
+        println("Stop")
+    }
+}*/
+
+//********* SEXTO PASO *******
+/*fun main() {
+    val trafficLightColor = "red"
+
+    if (trafficLightColor == "red") {
+        println("Stop")
+    } else {
+
+    }
+}*/
+
+// ********** septimo paso *********
+
+/*fun main() {
+    val trafficLightColor = "green"
+
+    if (trafficLightColor == "red"){
+        print("stop")
+    }
+
+    else {
+        print ("goo")
+    }
+    }*/
+
+// ******** OCTAVO PASO********
+
+/*fun main() {
+    val trafficLightColor = "yellow"
+
+    if (trafficLightColor == "red") {
+        print("stop")
+    }
+
+    else if (trafficLightColor == "yellow"){
+        print("Slow")
+    }
+
+        else {
+        print("goo")
+    }
+}*/
+
+//********* NOVENO PASO *************
+/*fun main() {
+    val trafficLightColor = "black"
+
+    if (trafficLightColor == "red") {
+        print("stop")
+    }
+
+    else if (trafficLightColor == "yellow"){
+        print("Slow")
+    }
+
+    else {
+        print("ningun color es valido")
+    }
+}*/
---
+++ src/punto2.kt
@@ -0,0 +1,176 @@
+// ***Primer punto***
+//fun main (){
+//    val favoriteActor = null
+//    print(favoriteActor)
+//}
+
+// *** SEGUNDO PUNTO ****
+//    fun main (){
+//
+//   var favoriteActor :String = "sandra ohh"
+//   print(favoriteActor)
+//
+//}
+
+//****TERCER PASO ****
+//    fun main (){
+//
+//        var favoriteActor :String = "sandra ohh"
+//       favoriteActor = null
+//
+//    }
+
+// *** CUARTO PASO ***
+//    fun main (){
+//
+//        var favoriteActor :String? = "sandra ohh"
+//        println(favoriteActor)
+//        println("actor favorito")
+//
+//    }
+
+// **QUINTO PASO**
+//    fun main (){
+//
+//  var number: Int? = 10
+//    print(number)
+//    }
+
+// ****SEXTO PASO***
+
+//fun main() {
+//    var number: Int? = 10
+//    println(number)
+//}
+// ** SEPTIMO PASO ***
+//fun main() {
+//    var number: Int? = 10
+//    println(number)
+//
+//    number = null
+//}
+
+// ** OCTAVO PASO ***
+
+//fun main() {
+//    var number: Int? = 10
+//    println(number)
+//
+//    number = null
+//    println(number)
+//}
+
+// ** NOVENO PASO ***
+
+//fun main() {
+//    var favoriteActor: String = "Sandra Oh"
+//}
+
+// ** DECIMO PASO ***
+
+//fun main() {
+//    var favoriteActor: String = "Sandra Oh"
+//    println(favoriteActor.length)
+//}
+
+// ** 11 PASO ***
+
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//    println(favoriteActor.length)
+//}
+
+// ** 12 PASO ***
+//
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//    println(favoriteActor?.length)
+//}
+
+// ** 13 PASO ***
+
+//fun main() {
+//    var favoriteActor: String? = null
+//    println(favoriteActor?.length)
+//}
+
+// ** 14 PASO ***
+
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//    println(favoriteActor!!.length)
+//}
+
+// ** 15 PASO ***
+
+//fun main() {
+//    var favoriteActor: String? = null
+//    println(favoriteActor!!.length)
+//}
+
+
+// ** 16 PASO ***
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//
+//    if (favoriteActor != null) {
+//
+//    }
+//}
+
+// ** 17 PASO ***
+
+//fun main() {
+//    var favoriteActor: String? = null
+//
+//    if(favoriteActor != null) {
+//        println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+//    } else {
+//        println("You didn't input a name.")
+//    }
+//}
+
+// ** 18 PASO ***
+
+//fun main() {
+//    var favoriteActor: String? = "Sandra Oh"
+//
+//    val lengthOfName = if(favoriteActor != null) {
+//        println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+//    } else {
+//        println("You didn't input a name.")
+//    }
+//}
+
+// ** 19 PASO ***
+
+//fun main() {
+//    val favoriteActor: String? = "Sandra Oh"
+//
+//    val lengthOfName = if(favoriteActor != null) {
+//        favoriteActor.length
+//    } else {
+//        0
+//    }
+//}
+
+// ** 20 PASO ***
+//fun main() {
+//    val favoriteActor: String? = "Sandra Oh"
+//
+//    val lengthOfName = if(favoriteActor != null) {
+//        favoriteActor.length
+//    } else {
+//        0
+//    }
+//
+//    println("The number of characters in your favorite actor//    val lengthOfName = favoriteActor?.length ?: 0
+////
+////    println("The number of characters in your favorite actor's name is $lengthOfName.")
+////}'s name is $lengthOfName.")
+//}
+
+// ** 21 PASO ***
+//fun main() {
+//    val favoriteActor: String? = "Sandra Oh"
+//
